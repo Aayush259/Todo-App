aaadf699dbc47c22a3a60257412ccd72dd227073
Square <aayushk4980@gmail.com>
2024-05-15 18:11:37 +0530

    Imported stylesheet & added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import "./styles/styles.css";
+import bgDesktopLight from "./images/bg-desktop-light.jpg";
+import bgDesktopDark from "./images/bg-desktop-dark.jpg";
+import bgMobileLight from "./images/bg-mobile-light.jpg";
+import bgMobileDark from "./images/bg-mobile-dark.jpg";
 
 function App() {
+
+  let backgroundImage = bgDesktopLight;
 
   return (
     <>
       <div id="app">
         Todo App
+        <div id="background">
+          <img src={backgroundImage} alt="Background" />
+        </div>
       </div>
     </>
   )
