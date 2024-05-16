aa4b89bfbfa9ddf7d0af500bc96ca211476815eb
Square <aayushk4980@gmail.com>
2024-05-16 16:28:54 +0530

    Update TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,17 +3,18 @@ import iconCross from "../images/icon-cross.svg";
 
 function TaskList(props) {
 
-    let [isTaskHovered, setTaskHoverState] = useState(null);
+    // Initializing taskHoverIndex state. When user will hover on any task then this state will be updated to the index of that task and removeTaskBtn will be displayed.
+    let [taskHoverIndex, setTaskHoverIndex] = useState(null);
 
     return (
         <>
         <div id="taskList">
             {props.taskList.map((task,index) => {
                 return (
-                <div className={`task ${props.theme} flex`} key={index} onMouseEnter={() => {setTaskHoverState(index)}} onMouseLeave={() => {setTaskHoverState(null)}}>
+                <div className={`task ${props.theme} flex`} key={index} onMouseEnter={() => {setTaskHoverIndex(index)}} onMouseLeave={() => {setTaskHoverIndex(null)}}>
                     <button id="addTaskBtn" className="leftTileBtn"></button>
                     <p style={{width: "90%"}}>{task}</p>
-                    {isTaskHovered === index && (<button id="removeTaskBtn"><img src={iconCross} alt="Remove Task" /></button>)}
+                    {taskHoverIndex === index && (<button id="removeTaskBtn"><img src={iconCross} alt="Remove Task" /></button>)}
                 </div>)
             })}
         </div>
