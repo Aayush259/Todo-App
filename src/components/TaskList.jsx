1453d315de56477b8e6441515b10270f358800a3
Square <aayushk4980@gmail.com>
2024-05-16 16:25:26 +0530

    Added removeTaskBtn in TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+import iconCross from "../images/icon-cross.svg";
 
 function TaskList(props) {
+
+    let [isTaskHovered, setTaskHoverState] = useState(null);
 
     return (
         <>
         <div id="taskList">
             {props.taskList.map((task,index) => {
                 return (
-                <div className={`task ${props.theme} flex`} key={index}>
+                <div className={`task ${props.theme} flex`} key={index} onMouseEnter={() => {setTaskHoverState(index)}} onMouseLeave={() => {setTaskHoverState(null)}}>
                     <button id="addTaskBtn" className="leftTileBtn"></button>
-                    <p style={{width: "100%"}}>{task}</p>
+                    <p style={{width: "90%"}}>{task}</p>
+                    {isTaskHovered === index && (<button id="removeTaskBtn"><img src={iconCross} alt="Remove Task" /></button>)}
                 </div>)
             })}
         </div>
