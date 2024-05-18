4aad0ba621b776a3fc7a12b4060cc150a7ecaee3
Square <aayushk4980@gmail.com>
2024-05-19 01:31:41 +0530

    Simplify App.jsx and Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 import "./styles/styles.css";
-import moonIcon from "./images/icon-moon.svg";
-import sunIcon from "./images/icon-sun.svg";
 
 function App() {
 
@@ -44,9 +42,6 @@ function App() {
   const addTask = (task) => {
     updateTaskList(prevTaskList => [...prevTaskList, task]);
   }
-
-  let themeLogo;
-  let themeAlt;
 
   // This function toggles the app theme when called.
   const changeTheme = () => {
@@ -88,22 +83,13 @@ function App() {
     document.body.className = theme;
   }, [theme]);
 
-  // If theme is light then set theme logo to moon icon and alt text accordingly else set the opposite logo and alt text.
-  if (theme === "light") {
-    themeLogo = moonIcon;
-    themeAlt = "Shift to dark mode";
-  } else {
-    themeLogo = sunIcon;
-    themeAlt = "Shift to light mode.";
-  }
-
   return (
     <>
       <div id="app">
         <div id="background" className={theme}>
         </div>
         <main id="main">
-          <Header themeLogo={themeLogo} themeAlt={themeAlt} changeTheme={changeTheme} />
+          <Header theme={theme} changeTheme={changeTheme} />
           <AddTask theme={theme} addTask={addTask} />
           <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd} sensors={sensors}>
             <TaskList taskList={taskList} theme={theme} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
+import moonIcon from "../images/icon-moon.svg";
+import sunIcon from "../images/icon-sun.svg";
 
 function Header(props) {
+
+    // Initializing themeLogo and themeAlt which will contain theme icon and its alternate text for image element in header.
+    let themeLogo;
+    let themeAlt;
+
+    // If theme is light then set theme logo to moon icon and alt text accordingly else set the opposite logo and alt text.
+    if (props.theme === "light") {
+        themeLogo = moonIcon;
+        themeAlt = "Shift to dark mode";
+    } else {
+        themeLogo = sunIcon;
+        themeAlt = "Shift to light mode.";
+    }
 
     return (
         <>
         <header id="header" className="flex">
             <h1>Todo</h1>
             <button id="changeThemeBtn" onClick={props.changeTheme}>
-                <img src={props.themeLogo} alt={props.themeAlt} />
+                <img src={themeLogo} alt={themeAlt} />
             </button>
         </header>
         </>
